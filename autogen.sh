b5bde690088d83a7eb28a99e698c9d75c2f3e74f
John Slade <john@jtes.net>
2011-08-15 22:59:00 +0100

    Script to autogen the code

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# TODO put this into scons
+
+python eventc_autogen.py Thread1_pri.h Thread1_autogen.c Thread1_autogen.h
+python eventc_autogen.py Thread2_pri.h Thread2_autogen.c Thread2_autogen.h
+
